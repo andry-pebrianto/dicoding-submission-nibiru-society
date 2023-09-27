07e17386d2289d2680e54a17b29e642066988184
Andry Pebrianto <andrypeb227@gmail.com>
2023-09-27 19:43:31 +0700

    Create AddThread, AddedThread entities and ThreadRepository

---
+++ src/Domains/threads/entities/_test/AddedThread.test.js
@@ -0,0 +1,47 @@
+const AddedThread = require("../AddedThread");
+
+describe("a AddedThread entities", () => {
+  it("should throw error when payload did not contain needed property", () => {
+    // Arrange
+    const payload = {
+      id: "ini id",
+      title: "ini title",
+    };
+
+    // Action and Assert
+    expect(() => new AddedThread(payload)).toThrowError(
+      "ADDED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY"
+    );
+  });
+
+  it("should throw error when payload did not meet data type specification", () => {
+    // Arrange
+    const payload = {
+      id: "ini id",
+      title: 111,
+      owner: "ini body",
+    };
+
+    // Action and Assert
+    expect(() => new AddedThread(payload)).toThrowError(
+      "ADDED_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION"
+    );
+  });
+
+  it("should create addedThread object correctly", () => {
+    // Arrange
+    const payload = {
+      id: "ini id",
+      title: "ini title",
+      owner: "ini body",
+    };
+
+    // Action
+    const { id, title, owner } = new AddedThread(payload);
+
+    // Assert
+    expect(id).toEqual(payload.id);
+    expect(title).toEqual(payload.title);
+    expect(owner).toEqual(payload.owner);
+  });
+});
