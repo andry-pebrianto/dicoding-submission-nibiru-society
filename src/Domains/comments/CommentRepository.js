12a6602d69d528739b38d7a685e2e204b21c84e9
Andry Pebrianto <andrypeb227@gmail.com>
2023-09-30 19:18:35 +0700

    Create CommentRepository and test

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,19 @@
+class CommentRepository {
+  async insertComment(dataComment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyCommentOwner(id, owner) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async softDeleteCommentById(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getCommentsByThreadId(threadId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
